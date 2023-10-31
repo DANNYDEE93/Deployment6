fff95089def818d441b93624a77bad0532315562
Ubuntu <ubuntu@ip-172-31-91-255.ec2.internal>
2023-10-31 20:25:35 +0000

    add changes to .py files, add main.tf and variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "key_name" {}
+
+variable "regionvpc1" {}
+variable "vpceast_cidr" {}
+variable "dep6publicSubnetA_cidr" {}
+variable "dep6publicSubnetB_cidr" {}
+
+variable "regionvpc2" {}
+variable "vpcwest_cidr" {}
+variable "dep6publicSubnetC_cidr" {}
+variable "dep6publicSubnetD_cidr" {}
