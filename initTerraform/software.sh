7d6720a290448c7ec2304892803543cd70bd17ef
Danielle Davis <140758597+DANNYDEE93@users.noreply.github.com>
2023-11-04 20:13:05 -0400

    Create software.sh

---
+++ initTerraform/software.sh
@@ -0,0 +1,44 @@
+
+#!bin/bash
+
+sudo apt-get update
+
+#installs ability to install, update, remove, and manage packages/ y for yes/ able to use apt for adding python dependencies
+sudo apt install -y software-properties-common
+
+#downloads updated versions of python 
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+
+#install python version
+sudo apt install -y python3.7
+
+#install python virtual environment
+sudo apt install -y python3.7-venv
+
+#installs essential build tools
+sudo apt install -y build-essential
+
+#installs the MySQL client library files
+sudo apt install -y libmysqlclient-dev
+
+#installs Python 3.7 files for app development
+sudo apt install -y python3.7-dev
+
+#create virtual environment
+python3.7 -m venv test
+source test/bin/activate
+git clone https://github.com/kura-labs-org/c4_deployment-5.git
+cd c4_deployment-5
+
+# Install Dependencies
+pip install -r requirements.txt
+pip install gunicorn
+pip install mysqlclient
+
+# Start Application
+python -m gunicorn app:app -b 0.0.0.0 -D
+
+#upgrade installed packages
+sudo apt upgrade
+
+
