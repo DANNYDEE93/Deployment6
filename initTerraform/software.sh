97d9387aa61cbb6c532be00bdc4f3394b05d7fab
Danielle Davis <140758597+DANNYDEE93@users.noreply.github.com>
2023-11-05 06:26:57 -0500

    Update software.sh

--- initTerraform/software.sh
+++ initTerraform/software.sh
@@ -26,16 +26,21 @@ sudo apt install -y python3.7-dev
 
 #create virtual environment
 python3.7 -m venv test
-source test/bin/activate
-git clone https://github.com/kura-labs-org/c4_deployment-5.git
-cd c4_deployment-5
 
-# Install Dependencies
+source test/bin/activate
+
+git clone https://github.com/DANNYDEE93/Deployment6.git
+
+cd Deployment6
+
+#install Dependencies
 pip install -r requirements.txt
+
 pip install gunicorn
+
 pip install mysqlclient
 
-# Start Application
+#start Application
 python -m gunicorn app:app -b 0.0.0.0 -D
 
 #upgrade installed packages
