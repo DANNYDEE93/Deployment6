ff3d29ba1f288f3ef8f803e9683909128bcb5c46
Danielle Davis <140758597+DANNYDEE93@users.noreply.github.com>
2023-11-12 18:28:01 -0500

    Update variables.tf

--- initTerraform/variables.tf
+++ initTerraform/variables.tf
@@ -3,18 +3,3 @@ variable "secret_key" {}
 variable "ami" {}
 variable "instance_type" {}
 variable "key_name" {}
-<<<<<<< HEAD
-
-=======
-variable "regiondefvpc" {}
-
-variable "regionvpc1" {}
-variable "vpceast_cidr" {}
-variable "dep6publicSubnetA_cidr" {}
-variable "dep6publicSubnetB_cidr" {}
-
-variable "regionvpc2" {}
-variable "vpcwest_cidr" {}
-variable "dep6publicSubnetC_cidr" {}
-variable "dep6publicSubnetD_cidr" {}
->>>>>>> 1e04ae357ac50996fa185d9f4a18295fd1b067b2
