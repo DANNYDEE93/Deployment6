df58fc80b85ac18aa9c97c361de917710b25e4cf
DANNYDEE93 <dan.dee5761@gmail.com>
2023-11-12 23:09:54 +0000

    push to repo

--- Setup_files/jenkins-deadsnakes.sh
+++ Setup_files/jenkins-deadsnakes.sh
@@ -1,7 +1,28 @@
-
 #!bin/bash
 
 sudo apt-get update
+
+#install java
+sudo apt install -y fontconfig -y openjdk-17-jre
+
+#download jenkins debian package
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -y jenkins
+
+sudo systemctl start jenkins
+
+######
+
+sudo apt-get update
+
+#######
 
 #installs ability to install, update, remove, and manage packages/ y for yes/ able to use apt for adding python dependencies
 sudo apt install -y software-properties-common
@@ -24,26 +45,5 @@ sudo apt install -y libmysqlclient-dev
 #installs Python 3.7 files for app development
 sudo apt install -y python3.7-dev
 
-#create virtual environment
-python3.7 -m venv test
-
-source test/bin/activate
-
-git clone https://github.com/DANNYDEE93/Deployment6.git
-
-cd Deployment6
-
-#install Dependencies
-pip install -r requirements.txt
-
-pip install gunicorn
-
-pip install mysqlclient
-
-#start Application
-python -m gunicorn app:app -b 0.0.0.0 -D
-
 #upgrade installed packages
 sudo apt upgrade
-
-
--- initTerraform/variables.tf
+++ initTerraform/variables.tf
@@ -3,6 +3,9 @@ variable "secret_key" {}
 variable "ami" {}
 variable "instance_type" {}
 variable "key_name" {}
+<<<<<<< HEAD
+
+=======
 variable "regiondefvpc" {}
 
 variable "regionvpc1" {}
@@ -14,3 +17,4 @@ variable "regionvpc2" {}
 variable "vpcwest_cidr" {}
 variable "dep6publicSubnetC_cidr" {}
 variable "dep6publicSubnetD_cidr" {}
+>>>>>>> 1e04ae357ac50996fa185d9f4a18295fd1b067b2
