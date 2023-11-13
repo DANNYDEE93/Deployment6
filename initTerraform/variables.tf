752d9f9660a4da4969c87c83f9e223034c651b69
Danielle Davis <140758597+DANNYDEE93@users.noreply.github.com>
2023-11-12 19:12:22 -0500

    Update variables.tf

--- initTerraform/variables.tf
+++ initTerraform/variables.tf
@@ -1,5 +1,5 @@
-variable "access_key" {}
-variable "secret_key" {}
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
 variable "ami" {}
 variable "instance_type" {}
 variable "key_name" {}
