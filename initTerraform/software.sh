f96819f0f8f78436217eba0c4d812958980d0095
Danielle Davis <140758597+DANNYDEE93@users.noreply.github.com>
2023-11-12 20:06:25 -0500

    Update software.sh

--- initTerraform/software.sh
+++ initTerraform/software.sh
@@ -1,51 +1,41 @@
-
 #!bin/bash
 
 sudo apt-get update
-
 #installs ability to install, update, remove, and manage packages/ y for yes/ able to use apt for adding python dependencies
-sudo apt install -y software-properties-common
-
+sudo apt-get install -y software-properties-common
 #downloads updated versions of python 
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-
+sudo apt-get update
 #install python version
-sudo apt install -y python3.7
-
+sudo apt-get install -y python3.7
 #install python virtual environment
-sudo apt install -y python3.7-venv
-
+sudo apt-get install -y python3.7-venv
 #installs essential build tools
-sudo apt install -y build-essential
-
+sudo apt-get install -y build-essential
 #installs the MySQL client library files
-sudo apt install -y libmysqlclient-dev
-
+sudo apt-get install -y libmysqlclient-dev
 #installs Python 3.7 files for app development
-sudo apt install -y python3.7-dev
+sudo apt-get install -y python3.7-dev
+
+
 
 #create python virtual environment
 python3.7 -m venv test
-
 source test/bin/activate
-
+#clone repo
 git clone https://github.com/DANNYDEE93/Deployment6.git
-
 cd Deployment6
-
+pip install pip --upgrade
 #install Dependencies
 pip install -r requirements.txt
-
-pip install -y gunicorn
-
-pip install -y mysqlclient
-
+pip install mysqlclient
+pip install gunicorn
 python database.py
-
 python load_data.py
-
 #start Application
 python -m gunicorn app:app -b 0.0.0.0 -D
+source test/bin/activate
+
 
 
 
