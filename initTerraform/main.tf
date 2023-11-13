de9d0b0ecb3649a844847205b158d3254c1a0933
Danielle Davis <140758597+DANNYDEE93@users.noreply.github.com>
2023-11-12 19:12:57 -0500

    Update main.tf

--- initTerraform/main.tf
+++ initTerraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   alias = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region = "us-east-1"
 }
 
@@ -168,8 +168,8 @@ output "applicationServer02-east_ip" {
 #
 provider "aws" {
   alias = "us-west-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
   region = "us-west-2"
 }
 
